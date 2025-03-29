a0d4b0d865146a56e3d89c1709bfc2b9e31fe317
kusukaze <kusukaze@163.com>
2025-03-29 18:26:42 +0800

    upload

--- src/main/resources/sql/queries.sql
+++ src/main/resources/sql/queries.sql
@@ -43,3 +43,12 @@ with jmdict_tmp as (
 SELECT * FROM jmdict_tmp
 WHERE binary yomi = reverse(yomi)
 and char_length(yomi) >= 4;
+
+-- 以い结尾的二类形容词
+with jmdict_tmp as (
+    select ID,termName,substring_index(yomi,',',1) as yomi,partOfSpeech from j.jmdict
+)
+SELECT * FROM jmdict_tmp
+WHERE binary termName like "%い"
+and partOfSpeech like "%adj-na%"
+and partOfSpeech not like "%adj-nari%";
